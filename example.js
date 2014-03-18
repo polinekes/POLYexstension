c847f5ffe714555318b53d0c8d8efd4842eecb8b
polinekes <polinekes_5@hotmail.com>
2014-03-18 13:55:22 +0200

    גרסא ראשונה
    לחיצה  1 שקיפות נמוכה ודאבל קליק נעלם

---
+++ example.js
@@ -0,0 +1,14 @@
+$( "p" ).click(function() {
+  $( this ).toggleClass( "opacity" );
+});
+
+
+$( "p" ).dblclick(function(){
+  $( this ).hide();
+});
+
+   // one click will make the opacity lower and
+//   dblclick will make the p disappear
+
+//  works only on http
+
